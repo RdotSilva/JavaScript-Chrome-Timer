59361abcd82f724ebc6617b5f77aa1c19b3a8c21
RdotSilva <RyandotSilva@gmail.com>
2021-09-06 15:07:33 -0400

    Add renderTask function

--- popup/popup.js
+++ popup/popup.js
@@ -1,23 +1,41 @@
+const tasks = [];
+
 const addTaskButton = document.getElementById("add-task-btn");
 addTaskButton.addEventListener("click", () => addTask());
 
 /**
- * Adds a new task input element with a button
+ * Render a single task
+ * @param {String} taskNum the number of the task to render
  */
-function addTask() {
+function renderTask(taskNum) {
   const taskRow = document.createElement("div");
 
   const text = document.createElement("input");
   text.type = "text";
   text.placeholder = "Enter a task...";
+  text.addEventListener("change", () => {
+    tasks[taskNum] = text.value;
+  });
 
   const deleteButton = document.createElement("input");
   deleteButton.type = "button";
   deleteButton.value = "X";
+  deleteButton.addEventListener("click", () => {
+    deleteTask(taskNum);
+  });
 
   taskRow.appendChild(text);
   taskRow.append(deleteButton);
 
   const taskContainer = document.getElementById("task-container");
   taskContainer.appendChild(taskRow);
+}
+
+/**
+ * Adds a new task input element with a button
+ */
+function addTask() {
+  const taskNum = tasks.length;
+  tasks.push("");
+  renderTask(taskNum);
 }
